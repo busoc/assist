edb72571e855139ed1f7e2ad8fb058a349adbc08
midbel <brun.nicolas21@gmail.com>
2020-12-08 14:00:53 +0100

    remove most of the command line options

--- help.go
+++ help.go
@@ -2,7 +2,7 @@ package main
 
 const helpText = `ASIM Semi Automatic Schedule Tool
 
-Usage: assist [options] <trajectory.csv>
+Usage: assist [options] <config.toml>
 
 Command files:
 
@@ -42,10 +42,6 @@ Eclipse are: 0, off, false
 
 Configuration sections/options:
 
-assist accepts via the "config" flag a configuration file as first argument
-instead of a file with a predicted trajectory. The format of the configuration
-file is toml.
-
 There are three main sections in the configuration files (options for each section
 are described below - check also the Options section of this help for additional
 information):
@@ -70,87 +66,21 @@ information):
   - cer-after-roc  = time after ROCON/ROCOFF to schedule a CEROFF
   - crossing       = mininum time of SAA and Eclipse
   - saa            = mininum SAA duration to have an AZM scheduled
+  - acs-time       = ACS expected execution time
+  - acs-night      = ACS minimum night duration
 
 * commands: configuring the location of the files that contain the commands
   - rocon  = file with commands for ROCON in text format
   - rocoff = file with commands for ROCOFF in text format
   - ceron  = file with commands for CERON in text format
   - ceroff = file with commands for CEROFF in text format
+  - acson  = file with commands for ACSON in text format
+  - acsoff = file with commands for ACSOFF in text format
 
 Options:
 
-  -rocon-time     TIME  ROCON expected execution time
-  -rocoff-time    TIME  ROCOFF expected execution time
-  -rocon-wait     TIME  wait TIME after entering Eclipse before starting ROCON
-  -roc-margin     TIME  margin time between ROCON end and ROCOFF start
-  -cer-time       TIME  TIME before Eclipse to switch CER(ON|OFF)
-  -cer-crossing   TIME  minimum crossing time of SAA and Eclipse to switch CER(ON|OFF)
-  -cer-before     TIME  schedule CERON TIME before entering SAA during eclipse
-  -cer-after      TIME  schedule CEROFF TIME before leaving SAA during eclipse
-  -cer-before-roc TIME  delay CERON before ROC when conflict
-  -cer-after-roc  TIME  delay CEROFF after ROC when conflict
-  -azm            TIME  AZM duration
-  -saa            TIME  SAA duration
-  -acs-time       TIME  ACS expected execution time
-  -acs-night      TIME  ACS minimum night duration
-  -rocon-file     FILE  use FILE with commands for ROCON
-  -rocoff-file    FILE  use FILE with commands for ROCOFF
-  -ceron-file     FILE  use FILE with commands for CERON
-  -ceroff-file    FILE  use FILE with commands for CEROFF
-  -acson-file     FILE  use FILE with commands for ACSON
-  -acsoff-file    FILE  use FILE with commands for ACSOFF
-  -resolution     TIME  TIME interval between two rows in the trajectory
-  -base-time      DATE
-  -alliop         FILE  save schedule to FILE
-  -instrlist      FILE  save instrlist to FILE
-  -keep-comment         keep comment (if any) from command files
-  -list-periods         print the list of eclipses and crossing periods
-  -list                 print the list of commands instead of creating a schedule
-  -ignore               keep schedule entries from block that does not meet constraints
-  -config               load settings from a configuration file
-  -version              print assist version and exit
-  -help                 print this message and exit
-
-Examples:
-
-# create a full schedule keeping the comments from the given command files with
-# a longer AZM for ROCON/ROCOFF and a larger margin for CERON/CEROFF
-# this command enable the classical algorithm to schedule CERON/CEROFF
-$ assist -keep-comment -base-time 2018-11-19T12:35:00Z \
-  -rocon-file /usr/local/etc/asim/MXGS-ROCON.txt \
-  -rocoff-file /usr/local/etc/asim/MXGS-ROCOFF.txt \
-  -ceron-file /usr/local/etc/asim/MMIA-CERON.txt \
-  -ceroff-file /usr/local/etc/asim/MMIA-CEROFF.txt \
-  -azm 80s \
-  -cer-time 900s \
-  -alliop /var/asim/2018-310/alliop.txt \
-  -instrlist /var/asim/2018-310/instrlist.txt \
-  inspect-trajectory.csv
-
-# create a schedule only for CER (the same can be done for ROC).
-# this command enable the classical algorithm to schedule CERON/CEROFF
-$ assist -keep-comment -base-time 2018-11-19T12:35:00Z \
-  -ceron-file /usr/local/etc/asim/MMIA-CERON.txt \
-  -ceroff-file /usr/local/etc/asim/MMIA-CEROFF.txt \
-  -cer-time 900s \
-  -alliop /var/asim/CER-2018-310/alliop.txt \
-  inspect-trajectory.csv
-
-# Same command as previous but this time CERON/CEROFF will be scheduled according
-# to the new algorithm (using the default value for the cer-before, cer-after,...
-# options).
-$ assist -keep-comment -base-time 2018-11-19T12:35:00Z \
-  -ceron-file /usr/local/etc/asim/MMIA-CERON.txt \
-  -ceroff-file /usr/local/etc/asim/MMIA-CEROFF.txt \
-  -alliop /var/asim/CER-2018-310/alliop.txt \
-  inspect-trajectory.csv
-
-# print the list of commands that could be scheduled from a local file
-$ assist -list tmp/inspect-trajectory.csv
-
-# print the list of commands that could be scheduled from the output of inspect
-$ inspect -d 24h -i 10s -f csv /tmp/tle-2018305.txt | assist -list
-
-# use a configuration file instead of command line options
-$ assist -config /usr/local/etc/asim/cerroc-ops.toml
+  -list-periods  print the list of eclipses and crossing periods
+  -list-entries  print the list of commands instead of creating a schedule
+  -version       print assist version and exit
+  -help          print this message and exit
 `
--- main.go
+++ main.go
@@ -53,39 +53,12 @@ func main() {
 		AcsNight:     Duration{time.Second * 180},
 		AcsTime:      Duration{time.Second * 5},
 	}
-	flag.Var(&d.Rocon, "rocon-time", "ROCON execution time")
-	flag.Var(&d.Rocoff, "rocoff-time", "ROCOFF execution time")
-	flag.Var(&d.Ceron, "ceron-time", "CERON execution time (not used to scheduled CER)")
-	flag.Var(&d.Ceroff, "ceroff-time", "CEROFF execution time (not used to scheduled CER)")
-	flag.Var(&d.Wait, "rocon-wait", "wait time before starting ROCON")
-	flag.Var(&d.Cer, "cer-time", "schedule CER TIME before entering eclipse")
-	flag.Var(&d.Intersect, "cer-crossing", "intersection time to enable CER")
-	flag.Var(&d.CerBefore, "cer-before", "schedule CERON before SAA")
-	flag.Var(&d.CerAfter, "cer-after", "schedule CEROFF after SAA")
-	flag.Var(&d.CerBeforeRoc, "cer-before-roc", "schedule CERON before ROC when conflict")
-	flag.Var(&d.CerAfterRoc, "cer-after-roc", "schedule CEROFF after ROC when conflict")
-	flag.Var(&d.AZM, "azm", "default AZM duration")
-	flag.Var(&d.Margin, "roc-margin", "ROC margin")
-	flag.Var(&d.Saa, "saa", "default SAA duration")
-	flag.Var(&d.AcsTime, "acs-time", "execution time for ACS command")
-	flag.Var(&d.AcsNight, "acs-night", "minimum time of night during aurora traversal")
-	flag.StringVar(&fs.Rocon, "rocon-file", "", "mxgs rocon command file")
-	flag.StringVar(&fs.Rocoff, "rocoff-file", "", "mxgs rocoff command file")
-	flag.StringVar(&fs.Ceron, "ceron-file", "", "mmia ceron command file")
-	flag.StringVar(&fs.Ceroff, "ceroff-file", "", "mmia ceroff command file")
-	flag.StringVar(&fs.Acson, "acson-file", "", "asim acson command file")
-	flag.StringVar(&fs.Acsoff, "acsoff-file", "", "asim acsoff command file")
-	flag.BoolVar(&fs.Keep, "keep-comment", false, "keep comment from command file")
-	flag.StringVar(&fs.Alliop, "alliop", "", "alliop file")
-	flag.StringVar(&fs.Instrlist, "instrlist", "", "instrlist file")
-	baseTime := flag.String("base-time", DefaultBaseTime.Format("2006-01-02T15:04:05Z"), "schedule start time")
-	resolution := flag.Duration("resolution", time.Second*10, "prediction accuracy")
-	config := flag.Bool("config", false, "use configuration file")
-	ignore := flag.Bool("ignore", false, "ignore hazarduous blocks")
-	elist := flag.Bool("list", false, "schedule list")
-	plist := flag.Bool("list-periods", false, "periods list")
-	ingest := flag.Bool("ingest", false, "")
-	version := flag.Bool("version", false, "print version and exists")
+	var (
+		baseTime = flag.String("base-time", DefaultBaseTime.Format("2006-01-02T15:04:05Z"), "schedule start time")
+		elist    = flag.Bool("list-entries", false, "schedule list")
+		plist    = flag.Bool("list-periods", false, "periods list")
+		version  = flag.Bool("version", false, "print version and exists")
+	)
 	flag.Parse()
 
 	if *version {
@@ -100,29 +73,16 @@ func main() {
 	if b.IsZero() {
 		b = DefaultBaseTime
 	}
-	var s *Schedule
-	if *config {
-		s, err = loadFromConfig(flag.Arg(0), &d, &fs, *ingest)
-	} else {
-		switch flag.NArg() {
-		default:
-			s, err = Open(flag.Arg(0), *resolution, NewArea())
-			if err == nil {
-				fs.Path = flag.Arg(0)
-			}
-		case 0:
-			s, err = OpenReader(os.Stdin, *resolution, NewArea())
-		}
-	}
+	s, err := loadFromConfig(flag.Arg(0), &d, &fs)
 	if err != nil {
 		Exit(err)
 	}
-	if *plist && !*ingest {
+	if *plist {
 		ListPeriods(s, b)
 		return
 	}
-	if *elist && !*ingest {
-		if err := ListEntries(s, b, d, fs, *ignore); err != nil {
+	if *elist {
+		if err := ListEntries(s, b, d, fs, false); err != nil {
 			Exit(err)
 		}
 		return
@@ -134,6 +94,7 @@ func main() {
 
 	var (
 		w      io.Writer
+		es     []*Entry
 		digest = md5.New()
 	)
 	switch f, err := os.Create(fs.Alliop); {
@@ -146,18 +107,7 @@ func main() {
 	default:
 		Exit(checkError(err, nil))
 	}
-	var es []*Entry
-	if files := flag.Args(); *ingest {
-		if len(files) <= 1 {
-			Exit(badUsage("no files to ingest"))
-		}
-		if *config {
-			files = files[1:]
-		}
-		es, err = ingestFiles(files, b)
-	} else {
-		es, err = s.Filter(b).Schedule(d, fs.CanROC(), fs.CanCER(), fs.CanACS())
-	}
+	es, err = s.Filter(b).Schedule(d, fs.CanROC(), fs.CanCER(), fs.CanACS())
 	if err != nil {
 		Exit(checkError(err, nil))
 	}
@@ -192,7 +142,7 @@ func main() {
 	}
 }
 
-func loadFromConfig(file string, d *delta, fs *fileset, ingest bool) (*Schedule, error) {
+func loadFromConfig(file string, d *delta, fs *fileset) (*Schedule, error) {
 	r, err := os.Open(file)
 	if err != nil {
 		return nil, checkError(err, nil)
@@ -221,9 +171,7 @@ func loadFromConfig(file string, d *delta, fs *fileset, ingest bool) (*Schedule,
 		return nil, badUsage(fmt.Sprintf("invalid configuration file: %v", err))
 	}
 	fs.Path, fs.Alliop, fs.Instrlist, fs.Keep = c.Trajectory, c.Alliop, c.Instr, c.Comment
-	if ingest {
-		return nil, nil
-	}
+
 	cs := make([]Shape, 0, len(c.Area.Boxes))
 	for _, r := range c.Area.Boxes {
 		cs = append(cs, r)
